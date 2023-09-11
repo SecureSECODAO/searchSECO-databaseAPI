6f81d95da79798a43a638c710ee0e0e003e26330
xray27 <wouter.beffers@gmail.com>
2023-09-11 20:20:32 +0200

    replaced spinlock with event driven protocol

--- src/Client.ts
+++ src/Client.ts
@@ -2,6 +2,7 @@ import { Socket } from 'net';
 import { MethodResponseData, ResponseDecoder, TCPResponse } from './Response';
 import { RequestType, TCPRequest, RequestGenerator } from './Request';
 import Logger, { Verbosity } from './searchSECO-logger/src/Logger';
+import EventEmitter from 'events'
 
 const MAX_RETRY_COUNT = 3;
 
@@ -18,19 +19,17 @@ export interface ITCPClient {
 	Execute(requestType: RequestType, data: string[]): Promise<TCPResponse>;
 }
 
-export class TCPClient implements ITCPClient {
+export class TCPClient extends EventEmitter implements ITCPClient {
 	private readonly _port: number = -1;
 	private readonly _host: string = '';
 	private readonly _clientName: string;
 	private readonly _client: Socket;
 	private _request: TCPRequest | undefined = undefined;
-	private _requestProcessed = false;
-	private _busy = false;
-	private _response: TCPResponse | undefined = undefined;
-	private _error: unknown | undefined = undefined;
 	private _retryCount = 0;
 
 	constructor(clientName: string, host: string, port: number | string, verbosity: Verbosity = Verbosity.DEBUG) {
+		super()
+
 		Logger.SetModule('database-API');
 		Logger.SetVerbosity(verbosity);
 
@@ -43,10 +42,12 @@ export class TCPClient implements ITCPClient {
 		this._host = host;
 
 		this._client = new Socket();
-		this._client.on('error', (err: unknown) => {
-			this._error = err;
-			this._requestProcessed = true;
-			this._busy = false;
+		this.initialize()
+	}
+
+	private initialize() {
+		this._client.on('error', (err) => {
+			this.emit('error', err)
 			this._client.destroy();
 		});
 		this._client.on('data', (data: string) => {
@@ -55,17 +56,14 @@ export class TCPClient implements ITCPClient {
 
 			if (Number.isNaN(parseInt(code))) {
 				Logger.Error(data, Logger.GetCallerLocation());
-				this._requestProcessed = true;
-				this._busy = false;
-				this._response = new TCPResponse(500, type, []);
+				this.emit('error', data)
 				return;
 			}
 
 			this._retryCount = 0;
-			this._requestProcessed = true;
-			this._busy = false;
 
-			this._response = new TCPResponse(
+
+			const response = new TCPResponse(
 				parseInt(code),
 				type,
 				ResponseDecoder.Decode(
@@ -74,41 +72,42 @@ export class TCPClient implements ITCPClient {
 				)
 			);
 
-			Logger.Debug(`Response code ${this._response.responseCode} received from database.`, Logger.GetCallerLocation());
+			Logger.Debug(`Response code ${response.responseCode} received from database.`, Logger.GetCallerLocation());
 
-			switch (this._response.responseCode) {
+			switch (response.responseCode) {
 				case 200: {
 					const isMessage = ((res: string | undefined) => {
 						return res && !res.includes('?') && Number.isNaN(parseInt(res));
-					})((this._response.response[0] as { raw: string } | undefined)?.raw);
+					})((response.response[0] as { raw: string } | undefined)?.raw);
 
-					if (isMessage) Logger.Info((this._response.response[0] as { raw: string }).raw, Logger.GetCallerLocation());
+					if (isMessage) Logger.Info((response.response[0] as { raw: string }).raw, Logger.GetCallerLocation());
 					break;
 				}
 				case 400:
 					Logger.Error(
-						`Bad request: ${(this._response.response[0] as { raw: string } | undefined)?.raw || 'error 400'}`,
+						`Bad request: ${(response.response[0] as { raw: string } | undefined)?.raw || 'error 400'}`,
 						Logger.GetCallerLocation()
 					);
 					break;
 				case 500:
 					Logger.Error(
-						`Server error: ${(this._response.response[0] as { raw: string } | undefined)?.raw || 'error 500'}`,
+						`Server error: ${(response.response[0] as { raw: string } | undefined)?.raw || 'error 500'}`,
 						Logger.GetCallerLocation()
 					);
 					break;
 				default:
 					Logger.Error(
-						`Unknown error: ${(this._response.response[0] as { raw: string }).raw}`,
+						`Unknown error: ${(response.response[0] as { raw: string }).raw}`,
 						Logger.GetCallerLocation()
 					);
 			}
 
+			this.emit('data', response)
 			this._client.destroy();
 		});
 	}
 
-	private _connect(): void {
+	private connect(): void {
 		this._client.connect(this._port, this._host);
 	}
 
@@ -155,37 +154,29 @@ export class TCPClient implements ITCPClient {
 	 * @returns A promise which resolves to a TCPResponse object.
 	 */
 	public async Execute(type: RequestType, data: string[]): Promise<TCPResponse> {
-		while (this._busy) await new Promise((resolve) => setTimeout(resolve, 500));
+		this.connect()
+		const request = RequestGenerator.Generate(type, this._clientName, data);
+		this.send(request)
 
-		this._busy = true;
-		this._connect();
-
-		this._requestProcessed = false;
-
-		this._request = RequestGenerator.Generate(type, this._clientName, data);
-
-		this._sendData(this._request);
-
-		while (!this._requestProcessed) await new Promise((resolve) => setTimeout(resolve, 500));
-
-		if (this._error) {
-			if (this._retryCount >= MAX_RETRY_COUNT) {
-				Logger.Error(`Connection timed out with error ${this._error}, skipping project`, Logger.GetCallerLocation());
-				this._retryCount = 0;
-				this._error = undefined;
-				return new TCPResponse(500, type, []);
-			}
-
-			Logger.Error(`Database Error: ${this._error}. Retrying after 2 seconds...`, Logger.GetCallerLocation());
-			this._error = undefined;
-			this._retryCount++;
-			await new Promise((resolve) => setTimeout(resolve, 2000));
-			await this.Execute(type, data);
-		}
-		return this._response || new TCPResponse(500, type, []);
+		return new Promise((resolve, reject) => {
+			this.on('error', async err => {
+				if (this._retryCount >= MAX_RETRY_COUNT) {
+					Logger.Error(`Connection timed out with error ${err}, skipping project`, Logger.GetCallerLocation());
+					reject(err)
+				}
+				else {
+					Logger.Error(`Database Error: ${err}. Retrying after 2 seconds...`, Logger.GetCallerLocation());
+					this._retryCount++;
+					await this.Execute(type, data);
+				}
+			})
+			this.on('data', (response: TCPResponse) => {
+				resolve(response)
+			})
+		})
 	}
 
-	private _sendData({ header, body }: TCPRequest) {
+	private send({ header, body }: TCPRequest) {
 		Logger.Debug(`Sending ${header.length} bytes to the database.`, Logger.GetCallerLocation());
 		Logger.Debug(`Sending: ${header}`, Logger.GetCallerLocation());
 		this._client.write(header);
